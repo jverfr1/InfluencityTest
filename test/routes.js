fef968b1620c24ad4ceb5e37384b683d65b2a690
jverfr1 <jverfr1@gmail.com>
2017-01-05 00:33:23 +0100

    status codes test added for both endpoints

--- test/routes.js
+++ test/routes.js
@@ -1,15 +1,35 @@
 
-var after = require('after');
-var should = require('should');
-var express = require('../')
-  , Route = express.Route
-  , methods = require('methods')
-  , assert = require('assert');
+// var after = require('after');
+// var should = require('should');
+// var express = require('../')
+//   , Router = express.Router
+//   , methods = require('methods')
+//   , assert = require('assert');
 
-describe('Route', function() {
-    it('should work without handlers', function(done) {
-      var req = { method: 'GET', url: '/' }
-      var route = new Route('/foo')
-      route.dispatch(req, {}, done)
-  })
-})
+// describe('Route', function() {
+//     it('should work without handlers', function(done) {
+//       var req = { method: 'GET', url: '/' }
+//       var router = new Router('/foo')
+//       route.dispatch(req, {}, done)
+//   })
+// })
+var supertest = require('supertest'),
+expect = require('chai').expect,
+app = supertest('localhost:3678');
+
+describe('Info', function(){
+	it('should return a 200 response', function(done) {
+		app.get('/api/log/list')
+		.expect(200,done);
+	});
+	it('should be an object', function(done) {
+		app.get('/api/user/TheRock')
+		.set('Accept','application/x-www-form-urlencoded')
+		.expect(200,done)
+		// .end(function(err,res){
+		// 	expect(res.body).to.have.property("data");
+		// 	expect(res.body.data).to.have.property("image");
+		// 	done();
+		// });
+	});
+});
