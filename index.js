b5bcba205fb6fb849ea2bb2943d1881ce4182b22
jverfr1 <jverfr1@gmail.com>
2017-01-05 13:03:46 +0100

    db url connection added to index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose     = require('mongoose');
 var app          = require('./app');
 var port         = process.env.PORT || 3678;
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/log', (err, res)=> {
+mongoose.connect(process.env.MONGODB_URI, (err, res)=> {
 	if (err) {
 		throw err;
 	}else {
